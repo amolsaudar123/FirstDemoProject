596e45a29146a82bd06768a050fd7c348ff62d4c
amolsaudar123 <amolsaudar123>
2017-10-01 00:53:59 +0530

    commit into repo

---
+++ grails-app/domain/connection/UserTransaction.groovy
@@ -0,0 +1,18 @@
+package connection
+
+class UserTransaction {
+
+    Integer amount
+    Account account
+    Date date
+    String tag
+    String type
+    String payee
+    String payer
+    String user
+
+
+    static constraints = {
+
+    }
+}
